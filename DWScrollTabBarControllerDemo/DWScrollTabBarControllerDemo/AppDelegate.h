d4cd247323d2914731790d867f969dcc3a65f8de
jiadawei <jiadawei80@126.com>
2017-04-05 21:37:35 +0800

    Initialize project.

---
+++ DWScrollTabBarControllerDemo/DWScrollTabBarControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DWScrollTabBarControllerDemo
+//
+//  Created by jiadawei on 05/04/2017.
+//  Copyright © 2017 david. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
